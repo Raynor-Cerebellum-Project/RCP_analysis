2b8d9f24a24ff49f9060182b3379410845cae1c7
Adam S C <adamsc@jhu.edu>
2025-07-17 09:52:17 -0400

    updated PCA template denoising

--- functions/neural/generate_template.m
+++ functions/neural/generate_template.m
@@ -2,20 +2,19 @@ function template = generate_template(chn_data, template_mode, repeat_boundaries
 % GENERATE_TEMPLATE
 % Creates pulse-by-pulse artifact templates from aligned channel data.
 
-if nargin < 5
-    pca_k = 3;
-end
 
-[NSTIM, interpulse_len] = size(chn_data);
-template = zeros(size(chn_data));
-w = linspace(0, 1, interpulse_len);
+if nargin < 5; pca_k = 3; end                   % Default # PCs
+
+
+[NSTIM, interpulse_len] = size(chn_data);       % Get size of data
+template = zeros(size(chn_data));               % Initialize the template to zeros
+w        = linspace(0, 1, interpulse_len);      % Create a vector of time-points to adjust for drift [temporary]
 
 % === Define first-pulse logical index using repeat_boundaries ===
 is_first_pulse = false(NSTIM, 1);
 is_first_pulse(repeat_boundaries(1:end-1) + 1) = true;
 
 switch lower(template_mode)
-
     case 'local'
         for i = 1:NSTIM
             if is_first_pulse(i)
@@ -59,6 +58,7 @@ switch lower(template_mode)
 
     case 'pca'
         for i = 1:NSTIM
+        %for i = 1:length(repeat_boundaries)
             if is_first_pulse(i)
                 prev_first_pulses = find(is_first_pulse(1:i-1));
                 n_available = numel(prev_first_pulses);
@@ -71,28 +71,33 @@ switch lower(template_mode)
                     drift = w * (-template_i(end));
                     template(i, :) = template_i + drift;
                 end
-                continue
-            end
-
-            % Use last up to 40 previous non-first pulses for PCA
-            prev_nonfirst = find(~is_first_pulse(1:i-1));
-            n_available = numel(prev_nonfirst);
-
-            if n_available > 0
-                n_use = min(39, n_available);
-                ref_idxs = prev_nonfirst(end - n_use + 1:end);
+             %   continue
+            elseif is_first_pulse(i-1)
+            % Use last up to 39 previous non-first pulses for PCA
+                
+                pulse_batch   = ref_idx(i:i+39);
+                
+                %prev_nonfirst = find(~is_first_pulse(1:i-1));
+                %n_available   = numel(prev_nonfirst);
+                %if n_available > 0
+                %    n_use = min(39, n_available);
+                %    ref_idxs = prev_nonfirst(end - n_use + 1:end);
+                
                 X = chn_data(ref_idxs, :);
                 [coeff, score, ~, ~, ~, mu] = pca(X);
 
-                actual_k = min(pca_k, size(score, 2));
-                recon = score(end, 1:actual_k) * coeff(:, 1:actual_k)' + mu;
-            else
-                recon = zeros(1, interpulse_len);  % no data yet
-            end
+                actual_k = min(pca_k, size(score, 2));   % Make sure you're not pulling too many PCs
+                recon    = score(end, 1:actual_k)*coeff(:, 1:actual_k)' + mu;
+                               
+                %else
+                %    recon = zeros(1, interpulse_len);  % no data yet
+                %end
 
-            template_i = recon - recon(1);
-            drift = w * (-template_i(end));
-            template(i, :) = template_i + drift;
+                template_i = bsxfun(@minus, recon, recon(1,:));
+                drift      = bsxfun(@times, w, (-template_i(end,:)));
+                template(i:i+39, :) = template_i + drift;
+            else
+            end
         end
 end
 end
